06fdea5de2ce11975c044c1f6fb6f2f60c0e6628
ig0r2 <yo.iggor@gmail.com>
2023-05-22 15:20:14 +0200

    Added File input listener

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+let fileInput = document.querySelector('input[type=file]');
+let filePreview = fileInput.previousElementSibling.querySelector('img');
+if (fileInput) {
+    fileInput.addEventListener('change', function () {
+        let reader = new FileReader();
+        reader.addEventListener('load', function () {
+            filePreview.src = reader.result;
+        });
+        reader.readAsDataURL(this.files[0]);
+    });
+}
