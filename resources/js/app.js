35cf088ab4ae931abb876e2b4246019a3adc9618
ig0r2 <yo.iggor@gmail.com>
2023-06-18 22:27:35 +0200

    Improvements

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,28 @@ import './bootstrap';
 
 let fileInput = document.querySelector('input[type=file]');
 if (fileInput) {
-    let filePreview = fileInput.previousElementSibling.querySelector('img');
-    fileInput.addEventListener('change', function () {
-        let reader = new FileReader();
-        reader.addEventListener('load', function () {
-            filePreview.src = reader.result;
-        });
-        reader.readAsDataURL(this.files[0]);
+  let filePreview = fileInput.previousElementSibling.querySelector('img');
+  fileInput.addEventListener('change', function () {
+    let reader = new FileReader();
+    reader.addEventListener('load', function () {
+      filePreview.src = reader.result;
     });
+    reader.readAsDataURL(this.files[0]);
+  });
+}
+
+let priceTotal = document.querySelector('#price-total');
+if (priceTotal) {
+  let price = document.querySelector('#price');
+  let quantity = document.querySelector('#number_of_tickets');
+
+  priceTotal.textContent = (
+    parseFloat(price.value) * parseInt(quantity.value)
+  ).toFixed(0);
+  
+  quantity.addEventListener('change', function () {
+    priceTotal.textContent = (
+      parseFloat(price.value) * parseInt(quantity.value)
+    ).toFixed(0);
+  });
 }
